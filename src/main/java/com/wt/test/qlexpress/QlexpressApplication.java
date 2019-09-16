d9839f6c6cad51bd8ed90bd305f45a72c8db862f
qiyu <jsj0804wt@126.com>
2019-09-16 11:01:09 +0800

    创建工程 by qiyu 2019-Sep-16

---
+++ src/main/java/com/wt/test/qlexpress/QlexpressApplication.java
@@ -0,0 +1,13 @@
+package com.wt.test.qlexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wt/test/qlexpress/demo/FirstQlexpress.java
@@ -0,0 +1,16 @@
+package com.wt.test.qlexpress.demo;
+
+import com.ql.util.express.ExpressRunner;
+
+/**
+ * @auther: 埼玉
+ * @date: 2019/9/16 10:56
+ * @description:
+ */
+public class FirstQlexpress {
+
+    public static void main(String[] args) {
+        ExpressRunner runner = new ExpressRunner();
+
+    }
+}
